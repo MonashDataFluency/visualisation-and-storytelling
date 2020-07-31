eefd67c485d39ba262f2ef51a2b203110a89471b
Nora <nora.hamacher@gmail.com>
2020-07-31 11:32:48 +1000

    Updates

---
+++ precache-manifest.af76a9e204694a57ab0397e8b655151c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d2e1ee0de78ff742dc90f89832e51f",
+    "url": "/visualisation-and-storytelling/index.html"
+  },
+  {
+    "revision": "c75872138847e7669563",
+    "url": "/visualisation-and-storytelling/static/css/main.32b86ac0.chunk.css"
+  },
+  {
+    "revision": "26160556720e1ebdadb8",
+    "url": "/visualisation-and-storytelling/static/js/2.63b2a197.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/visualisation-and-storytelling/static/js/2.63b2a197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75872138847e7669563",
+    "url": "/visualisation-and-storytelling/static/js/main.df1b9658.chunk.js"
+  },
+  {
+    "revision": "b380dd9b3571105d40e9",
+    "url": "/visualisation-and-storytelling/static/js/runtime-main.0ee429ba.js"
+  },
+  {
+    "revision": "8ef92bcf2593d0064bb78b0b16c50c93",
+    "url": "/visualisation-and-storytelling/static/media/finishedVis.8ef92bcf.jpg"
+  }
+]);
